dd27c71f1d74f4abbca4220e3569c4e95599fb5f
Pedro Lisboa <lisboa.pedrohenrique1@gmail.com>
2025-04-17 20:09:40 -0300

    Very buggy rendering. Trying to get projection distance.

--- render.py
+++ render.py
@@ -12,7 +12,9 @@ class Renderer:
         x_axis = np.linspace(0, self.scene_width, len(rays))
         for ray, x in zip(rays, x_axis):
             if ray.collide_point:
-                rect_height = self.map_value(ray.distance_to_collision, 0, self.scene_width*1.5, self.scene_height, 0)
+                
+                projection_distance = ray.distance_to_collision * np.cos(np.rad2deg(ray.angle))
+                rect_height = self.scene_height / projection_distance
                 rect = pygame.Rect(0,0,self.rect_width+10, rect_height)
                 rect.center = (x, self.scene_height/2)
                 self.rects.append(rect)
@@ -20,6 +22,7 @@ class Renderer:
 
     def draw(self):
         for rect in self.rects:
+            rect_color = self.map_value(rect.height, 1,self.scene_height, 0, 255)
             pygame.draw.rect(surface=self.surface, color="white", rect=rect)
 
     def map_value(self, x, in_min, in_max, out_min, out_max):
