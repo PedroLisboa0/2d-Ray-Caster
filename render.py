454443b3133e49f5e1083e03edab68bad38f072e
Pedro Lisboa <lisboa.pedrohenrique1@gmail.com>
2025-04-17 17:33:25 -0300

    Fixed bug in last commit.

--- render.py
+++ render.py
@@ -3,7 +3,7 @@ import pygame
 class Renderer:
     def __init__(self, surface):
         self.scene = surface
-        self.scene_width, self.scene_height = surface.get_window_size()
+        self.scene_width, self.scene_height = pygame.display.get_window_size()
 
     def create_scene(self, rays, walls):
         pass
